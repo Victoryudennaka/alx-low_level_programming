5e23e3fe325f4ddbdd34f0f176823df1bc79e41a
Victoryudennaka <udennakavictory@gmail.com>
2023-08-05 05:18:54 -0700

    update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,9 @@
 *@argv: array of argument
 *Return: 0
 */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-printf("%d\n", argc);
+(void) argv; /*Ignore argv*/
+printf("%d\n", argc - 1);
 return (0);
 }
