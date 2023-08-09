03680ab5dfd8271fc5c4d4ca31599bf628b053c4
Victoryudennaka <udennakavictory@gmail.com>
2023-08-09 08:20:54 -0700

    update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,43 +1,43 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * alloc_grid - nested loop to make grid
- * @width: width input
- * @height: height input
- * Return: pointer to 2 dim. array
- */
+*alloc_grid - the pointer to arrays
+*@width: the column
+*@height: the row
+*Return: the ptr to pointer
+*/
 int **alloc_grid(int width, int height)
 {
-	int **mee;
-	int x, y;
+int i;
+int j;
+int **ptr;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	mee = malloc(sizeof(int *) * height);
-
-	if (mee == NULL)
-		return (NULL);
-
-	for (x = 0; x < height; x++)
-	{
-		mee[x] = malloc(sizeof(int) * width);
-
-		if (mee[x] == NULL)
-		{
-			for (; x >= 0; x--)
-				free(mee[x]);
-
-			free(mee);
-			return (NULL);
-		}
-	}
-
-	for (x = 0; x < height; x++)
-	{
-		for (y = 0; y < width; y++)
-			mee[x][y] = 0;
-	}
-
-	return (mee);
+if (width <= 0 || height <= 0)
+return (NULL);
+ptr = calloc(height, sizeof(int *));
+if (ptr == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+ptr[i] = calloc(width, sizeof(int));
+if (ptr[i] == NULL)
+{
+for (; i >= 0; i--)
+{
+free(ptr[i]);
+free(ptr);
+}
+return (NULL);
+}
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+ptr[i][j] = 0;
+}
+}
+return (ptr);
 }
