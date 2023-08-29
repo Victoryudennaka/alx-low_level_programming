a2ada06911a0cc3afd1e52d4f934df5c690ca0a4
Victoryudennaka <udennakavictory@gmail.com>
2023-08-29 03:57:14 -0700

    update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+*print_listint - print and count the elements
+*@h: the head/pointer to the first node
+*
+*Return: the size
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t size = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+size++;
+}
+return (size);
+}
